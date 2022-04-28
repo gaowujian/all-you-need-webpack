d3852814c886112506ecd4b769664fc6abd961f7
Tony Gao <gwjlshebei@gmail.com>
2022-04-28 09:48:14 +0800

    use hmr

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { useState } from "react";
 // @ts-ignore
 import txtContent from "./assets/extra.txt";
 
 function home() {
   const name: string = "gaowujian";
-
+  const [value, setValue] = useState("default");
   return (
     <div className="home" style={{ overflow: "hidden" }}>
       <h1 className="home-title">姓名:{name}</h1>
-      <h1 className="home-title">姓名:{name}</h1>
+      <h1 className="home-title">age:{name}</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
 
       <img className="profile" src="profile.jpg" alt="头像" />
       <p>{txtContent}</p>
